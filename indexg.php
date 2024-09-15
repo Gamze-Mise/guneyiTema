9ddba1ba94c5a12db0f31b986696badfe0e64c9e
Gamze MİŞE <gamzegm8@gmail.com>
2024-09-15 17:35:25 +0300

    Update indexg.php

--- indexg.php
+++ indexg.php
@@ -828,42 +828,6 @@
     </section><!-- End Contact Section -->
 
 </main><!-- End #main -->
-<div  class="modal"  id="modalImageFlood"  tabindex="-1">
-    <div class="modal-dialog modal-xl">
-        <div class="modal-content">
-
-            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-                <div style="display: none;" class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div>
-                <div class="carousel-inner">
-                    <?php for ($i = 1; $i <= 4; $i++) { ?>
-                        <div class="carousel-item" id="carousel-item<?php echo $i;?>" >
-                            <img src="assets/img/flood-project/<?php echo $i;?>.jpeg" class="d-block w-100" alt="...">
-                            <div class="carousel-caption d-none d-md-block">
-                                <p style="background-color: rgba(0, 0, 0, 0.5)" id="id-careousel-p<?php echo $i;?>"> <?php echo $floodP[$i]; ?></p>
-                            </div>
-                        </div>
-                    <?php } ?>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-
-
 
 <!-- ======= Footer ======= -->
 <footer id="footer">
