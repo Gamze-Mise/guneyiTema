b7dad71346aa174014981b259dcdfd2d8f727a8d
Gamze MİŞE <gamzegm8@gmail.com>
2024-11-15 17:37:38 +0300

    Revert "first commti"
    This reverts commit 8c5671b92005b386e38e2f8ee1fc087c01b9cda2.

---
+++ calistir.php
@@ -0,0 +1,22 @@
+<?php
+require_once "include/kelime.donustur.php";
+$linkStr=isset($_GET["islem"]) ? $_GET["islem"] : "";
+define("PAGE_NAME","https://www.guneyivural.com/");
+
+if(substr($linkStr, -1)==="/"){
+    $linkStr=substr($linkStr, 0,-1);
+}
+$linkDizi=explode("/",$linkStr);
+
+$yedekLinkDizi=$linkDizi;
+
+foreach($linkDizi as $i=>$lKelime){
+    $linkDizi[$i]=kelimeDonustur($lKelime);
+}
+
+if(count($linkDizi)==1 || count($linkDizi)==0){
+    include "indexg.php";
+    exit();
+}
+header("Location: ".PAGE_NAME);
+exit();
